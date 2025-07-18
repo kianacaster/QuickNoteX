62ab5bb8f905da3ae26b19d9b20082996fc919e1
Kian Acaster <acasterkian@gmail,com>
2025-07-18 20:49:50 +0000

    Quick Fixes

--- launch_quicknotex.sh
+++ launch_quicknotex.sh
@@ -4,13 +4,15 @@
 # This software is licensed under the MIT License.
 # See LICENSE for more information.
 
-INPUT=$(/usr/bin/rofi -dmenu -theme /usr/share/rofi/themes/quicknote.rasi -width 100 -lines 1 -location 0 -yoffset -200 -p "QuickNoteX")
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+INPUT=$(/usr/bin/rofi -dmenu -theme "$SCRIPT_DIR/rofi_themes/quicknote.rasi" -width 100 -lines 1 -location 0 -yoffset -200 -p "QuickNoteX")
 
 if [ -n "$INPUT" ]; then
     if [ "$INPUT" = "/open" ]; then
-        NOTES=$(/usr/local/bin/quicknote /open)
-        echo -e "$NOTES" | /usr/bin/rofi -dmenu -theme /usr/share/rofi/themes/quicknote_display.rasi -width 100 -lines 10 -location 0 -p "Your Notes"
+        NOTES=$("$SCRIPT_DIR/Backend/quicknote" /open)
+        echo -e "$NOTES" | /usr/bin/rofi -dmenu -theme "$SCRIPT_DIR/rofi_themes/quicknote_display.rasi" -width 100 -lines 10 -location 0 -p "Your Notes"
     else
-        /usr/local/bin/quicknote "$INPUT"
+        "$SCRIPT_DIR/Backend/quicknote" "$INPUT"
     fi
 fi
